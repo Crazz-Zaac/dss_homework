3bf7fde3a59ed3b573d48e217d9d4cd51910bb29
crazz-zaac <r.alpha919@gmail.com>
2023-11-16 14:17:30 +0100

    setup files update with entry requirements

--- setup.py
+++ setup.py
@@ -4,7 +4,14 @@ setup(
     name='mathematics quiz',
     version='0.1.0',
     packages=find_packages(),
-
+    # install_requires=[
+    #     # List your package dependencies here if any
+    # ],
+    entry_points={
+        'console_scripts': [
+            'math_quiz = mathquiz.__main__:main',
+        ],
+    },
     # Additional metadata
     author='Rabin BK',
     description='Mathematical quiz game',
